34939e2734921f09b89b3bc79f8484a2aaa324f8
rajuahammedwd <raju720190r@gmail.com>
2024-11-08 12:17:02 +0600

    problem-4

---
+++ problem-4.ts
@@ -0,0 +1,29 @@
+{
+  //
+
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  }
+  type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  }
+  type Shape = Circle | Rectangle;
+  function calculateShapeArea(shape: Shape): number {
+    if (shape.shape === "circle") {
+      return Math.PI * shape.radius * shape.radius;
+    } else if (shape.shape === "rectangle") {
+      return shape.width * shape.height;
+    } else {
+      throw new Error("Shape Type not Found");
+    }
+  }
+//   
+  const circleArea = calculateShapeArea({shape: "circle", radius: 5})
+//   console.log(circleArea) 
+
+  const rectangleArea = calculateShapeArea({shape: "rectangle", width: 4, height:6})
+//   console.log(rectangleArea)
+}
